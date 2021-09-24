716910e3142bfa1396b03061b7223788292df779
Franck Pachot (YB) <33070466+FranckPachot@users.noreply.github.com>
2021-09-24 15:10:09 +0200

    Add optimizations for PostgreSQL and compatible databases
    * Update config.yaml
    
    * 1. adding the "sql" value to the "primary_key"
       in order to use a GENERATED ALWAYS sequence
       with CACHE (= batch size)
    2. changed == "db" to != "client"
    
    * added "primary_key_constraint" config to specify the primary key
    definition (useful for sharding) - default is PRIMARY KEY(id)
    
    * added the COPY method of postgres, by default when having a batch size
    but use_values_lists at false. This disallows running row-by-row with
    batch commit, but this doesn't make sense anyway: if we don't want to
    commint each row, why sending them one by one?
    
    * Revert "Update config.yaml"
    
    This reverts commit 450d3136999bf648d4da5ee4974dcb5f05bedb94.
    
    * fixed comments and README
    
    * Update documentation and option handling
    
    Co-authored-by: Franck Pachot <franck.pachot@dbi-services.com>
    Co-authored-by: Sebastian Woehrl <sebastian.woehrl@maibornwolff.de>

--- simulator/modules/postgres.py
+++ simulator/modules/postgres.py
@@ -2,6 +2,8 @@ import psycopg2
 import psycopg2.extras
 from .config import config
 
+import io
+
 
 def _db():
     connection_string = config["connection_string"]
@@ -24,16 +26,24 @@ def init():
         for table_name in ["events0", "events1", "events2", "events3", "events"]:
             cur.execute(f"""DROP TABLE IF EXISTS {table_name}""")
         db.commit()
-    pk_column = "serial" if config["primary_key"] == "db" else "varchar"
+    # "primary_key" can be "client" to generate the PK value from the application, or "db" to use the "serial"
+    #  or "sql" for the SQL standard generated column, and then the cache size is the batch size
+    if config["primary_key"] == "sql":
+     pk_column = f'bigint generated always as identity ( start with 1 cache {config.get("batch_size", 100)} )'
+    elif config["primary_key"] == "db":
+        pk_column = "serial"
+    else:
+        pk_column = "varchar" 
 
     for table_name in table_names:
         cur.execute(
         f"""
-        CREATE TABLE IF NOT EXISTS {table_name} (id {pk_column} PRIMARY KEY,
+        CREATE TABLE IF NOT EXISTS {table_name} (id {pk_column} ,
             timestamp bigint,
             device_id varchar,
             sequence_number bigint,
-            temperature real
+            temperature real,
+            {config.get("primary_key_constraint","PRIMARY KEY (id)")}
             )
         """)
     db.commit()
@@ -68,7 +78,7 @@ def _insert_events(events, batch_mode, batch_size, use_values_lists=False):
         count = 0
         values_lists = [list() for _ in range(4 if use_multiple_tables else 1)]
         for idx, event in enumerate(events):
-            if config["primary_key"] == "db":
+            if config["primary_key"] != "client":
                 val = (event.timestamp, event.device_id, event.sequence_number, event.temperature)
             else:
                 event_id = f"{event.device_id}{event.timestamp}{event.sequence_number}"
@@ -80,7 +90,7 @@ def _insert_events(events, batch_mode, batch_size, use_values_lists=False):
             count += 1
             if count >= batch_size:
                 for table_index, values in enumerate(values_lists):
-                    if config["primary_key"] == "db":
+                    if config["primary_key"] != "client":
                         psycopg2.extras.execute_values(cur, f"INSERT INTO {table_names[table_index]} (timestamp, device_id, sequence_number, temperature) VALUES %s", values)
                     else:
                         psycopg2.extras.execute_values(cur, f"INSERT INTO {table_names[table_index]} (id, timestamp, device_id, sequence_number, temperature) VALUES %s", values)
@@ -89,11 +99,45 @@ def _insert_events(events, batch_mode, batch_size, use_values_lists=False):
                 count = 0
         if count > 0:
             for table_index, values in enumerate(values_lists):
-                if config["primary_key"] == "db":
+                if config["primary_key"] != "client":
                     psycopg2.extras.execute_values(cur, f"INSERT INTO {table_names[table_index]} (timestamp, device_id, sequence_number, temperature) VALUES %s", values)
                 else:
                     psycopg2.extras.execute_values(cur, f"INSERT INTO {table_names[table_index]} (id, timestamp, device_id, sequence_number, temperature) VALUES %s", values)
             db.commit()
+    elif not use_values_lists and batch_mode: # This uses the COPY mode of Postgres, when in batch without a VALUES list
+        count = 0
+        # the values_lists here is a StringIO containing the TSV to COPY
+        values_lists = [io.StringIO() for _ in range(4 if use_multiple_tables else 1)]
+        for idx, event in enumerate(events):
+            if config["primary_key"] != "client": 
+                val = f'{event.timestamp}\t{event.device_id}\t{event.sequence_number}\t{event.temperature}\n' 
+            else:
+                event_id = f"{event.device_id}{event.timestamp}{event.sequence_number}"
+                val = f'{event_id}\t{event.timestamp}\t{event.device_id}\t{event.sequence_number}\t{event.temperature}\n' 
+            if use_multiple_tables:
+                values_lists[idx%4].writelines(val)
+            else:
+                values_lists[0].writelines(val)
+            count += 1
+            if count >= batch_size:
+                for table_index, values in enumerate(values_lists):
+                    values.seek(0)
+                    if config["primary_key"] != "client": 
+                        cur.copy_from(values,table_names[table_index],sep="\t",columns=('timestamp', 'device_id', 'sequence_number', 'temperature'))
+                    else:
+                        cur.copy_from(values,table_names[table_index],sep="\t",columns=('id','timestamp', 'device_id', 'sequence_number', 'temperature'))
+                    values.seek(0)
+                    values.truncate(0)
+                db.commit()
+                count = 0
+        if count > 0:
+                for table_index, values in enumerate(values_lists):
+                    values.seek(0)
+                    if config["primary_key"] != "client": 
+                        cur.copy_from(values,table_names[table_index],sep="\t",columns=('timestamp', 'device_id', 'sequence_number', 'temperature'))                   
+                    else:
+                        cur.copy_from(values,table_names[table_index],sep="\t",columns=('id','timestamp', 'device_id', 'sequence_number', 'temperature'))
+                db.commit()    
     else:
         count = 0
         for idx, event in enumerate(events):
@@ -102,7 +146,7 @@ def _insert_events(events, batch_mode, batch_size, use_values_lists=False):
             else:
                 table_name = "events"
 
-            if config["primary_key"] == "db":
+            if config["primary_key"] != "client":
                 cur.execute(f"INSERT INTO {table_name} (timestamp, device_id, sequence_number, temperature) VALUES (%s, %s, %s, %s)",
                         (event.timestamp, event.device_id, event.sequence_number, event.temperature))
             else:
